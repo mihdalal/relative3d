9d5b4c736f238a67e842f95d349e305383d4ff98
Murtaza <mdalal@dgx-1.ml.cmu.edu>
2021-04-08 16:05:02 -0400

    updated camera views and light strenght

--- renderer/render_image_from_obj.py
+++ renderer/render_image_from_obj.py
@@ -14,19 +14,40 @@ if __name__ == '__main__':
 
     camObj = bpy.data.objects["Camera"]
 
-    # view 1
-    camObj.location[0] = 112.389
-    camObj.location[1] = 107.188
-    camObj.location[2] = 155.243
-    camObj.rotation_mode = "QUATERNION"
-    camObj.rotation_quaternion[0] = 0.043111
-    camObj.rotation_quaternion[1] = 0.036924
-    camObj.rotation_quaternion[2] = 0.649456
-    camObj.rotation_quaternion[3] = 0.758277
+    if camera_view == 1:
+        # top down
+        camObj.location[0] = 112.389
+        camObj.location[1] = 107.188
+        camObj.location[2] = 155.243
+        camObj.rotation_mode = "QUATERNION"
+        camObj.rotation_quaternion[0] = 0.043111
+        camObj.rotation_quaternion[1] = 0.036924
+        camObj.rotation_quaternion[2] = 0.649456
+        camObj.rotation_quaternion[3] = 0.758277
+    elif camera_view == 2:
+        #left view
+        camObj.location[0] = 240.244
+        camObj.location[1] = 89.0999
+        camObj.location[2] = 70.8328
+        camObj.rotation_mode = "QUATERNION"
+        camObj.rotation_quaternion[0] = 0.147761
+        camObj.rotation_quaternion[1] = 0.166308
+        camObj.rotation_quaternion[2] = 0.728829
+        camObj.rotation_quaternion[3] = 0.647547
+    else:
+        #right view
+        camObj.location[0] = 59.5978
+        camObj.location[1] = -7.64276
+        camObj.location[2] = -76.8625
+        camObj.rotation_mode = "QUATERNION"
+        camObj.rotation_quaternion[0] = 0.017829
+        camObj.rotation_quaternion[1] = 0.036848
+        camObj.rotation_quaternion[2] = -0.89941
+        camObj.rotation_quaternion[3] = -0.435185
 
     world = bpy.data.worlds["World"]
     world.use_nodes = True
-    world.node_tree.nodes["Background"].inputs["Strength"].default_value = 7.5
+    world.node_tree.nodes["Background"].inputs["Strength"].default_value = 12.5
 
     camera = bpy.context.scene.camera
     camera.data.clip_start = 0.01
